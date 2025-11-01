80c7a167c13a046ee01492f77804ab26d04b6498
Ruminov <74424348+Ruminov@users.noreply.github.com>
2025-10-31 22:32:12 -0400

    style(yazi): change window size

--- lua/custom/plugins/yazi.lua
+++ lua/custom/plugins/yazi.lua
@@ -3,41 +3,21 @@ return {
     'mikavilpas/yazi.nvim',
     event = 'VeryLazy',
     keys = {
-      -- 👇 in this section, choose your own keymappings!
-      {
-        '<leader>yf',
-        mode = { 'n', 'v' },
-        '<cmd>Yazi<cr>',
-        desc = 'Open yazi at the current file',
-      },
       {
         -- Open in the current working directory
-        -- '<cmd>Yazi cwd<cr>',
         '<leader>e',
         '<cmd>Yazi cwd<cr>',
         desc = "Open the file manager in nvim's working directory",
-      },
-      {
-        '<leader>yr',
-        '<cmd>Yazi toggle<cr>',
-        desc = 'Resume the last yazi session',
       },
     },
 
     opts = {
       -- if you want to open yazi instead of netrw, see below for more info
       open_for_directories = false,
-      -- REFACTOR:\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-      floating_window_scaling_factor = 1.0,
-      -- floating_window_scaling_factor = 0.99,
-      yazi_floating_window_border = 'solid',
+      floating_window_scaling_factor = 0.99,
+      yazi_floating_window_border = 'none',
       -- yazi_floating_window_zindex = 51,
 
-      -- EXPERIMENT:\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-      -- the zindex of the yazi floating window. Can be used to make the yazi
-      -- window fullscreen. See `:h nvim_open_win()` for more information.
-      -- EXPERIMENT://///////////////////////////////////////////////////
-      -- REFACTOR://///////////////////////////////////////////////////
       keymaps = {
         show_help = '<f1>',
         open_file_in_vertical_split = '<c-v>',
@@ -54,14 +34,8 @@ return {
         -- before yazi has been closed. If this is `false`, events are processed
         -- in a batch when the user closes yazi. If this is `true`, events are
         -- processed immediately.
-        process_events_live = false,
+        -- process_events_live = false,
       },
     },
-    -- 👇 if you use `open_for_directories=true`, this is recommended
-    init = function()
-      -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-      -- vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
-    end,
   },
 }
